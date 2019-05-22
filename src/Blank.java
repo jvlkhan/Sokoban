b97de8bd7ab265812cca661f9d32d63bf113143a
MKL <MKL@192.168.236.198>
2019-05-22 16:12:56 +0200

    commented code

--- src/Blank.java
+++ src/Blank.java
@@ -5,11 +5,13 @@ import FrameWork.Actor;
 
 public class Blank extends Actor {
 	public Blank(int x, int y) {
+		// refers to parent's constructor
 		super(x, y);
 		blankInit();
 	}
 
 	private void blankInit() {
+		// initiates the blank spot on the game
 		ImageIcon Icon = new ImageIcon("images/blank.png");
 		Image image = Icon.getImage();
 		setImage(image);
